7dbbfbbba49b6866b8229678fbf2a7db8a80a10d
Nikolay <nikolay.dubina.pub@gmail.com>
2020-12-24 17:20:43 +0800

    fifo reopen slow

--- xgb_c_fifo_go/fifo_test.go
+++ xgb_c_fifo_go/fifo_test.go
@@ -4,12 +4,13 @@ import (
 	"encoding/binary"
 	"fmt"
 	"os"
+	"os/exec"
 	"syscall"
 	"testing"
 )
 
-const fifoFeaturesPath = "../features.fifo"
-const fifoPredictionsPath = "../predictions.fifo"
+const fifoFeaturesPath = "features.fifo"
+const fifoPredictionsPath = "predictions.fifo"
 
 func predict(features []float32) (float32, error) {
 	fifoFeatures, err := os.OpenFile(fifoFeaturesPath, os.O_WRONLY, 0666)
@@ -43,6 +44,20 @@ func BenchmarkFifoXGBoostC(b *testing.B) {
 	syscall.Mkfifo(fifoPredictionsPath, 0666)
 
 	features := []float32{72.0, 72.0, 69.0, 60.0, 7.0, 0, 0, 0, 1, 1, 0, 1, 0}
+
+	// Start XGBoost C server listening to UNIX pipes
+	cmd := exec.Command("make", "run", "-C", "xgboost")
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	// Kill XGBoost C server once finish
+	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	if err != nil {
+		panic(err)
+	}
+	defer syscall.Kill(-pgid, 15)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
